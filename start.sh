7d2ac18fe0aaa178fd0126e4f0dc779ebd11f9c7
aleksei-roshchin <78482140+aleksei-roshchin@users.noreply.github.com>
2023-04-17 13:27:36 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ echo "IP ADDRESS:"
 hostname -i
 
 # Store the licese file inside the container for later reuse.
-cp /opt/rlm/licenses/speedtree.lic /opt/speedtree.lic
+# cp /opt/rlm/licenses/speedtree.lic /opt/speedtree.lic
 
 # Run the license server directly with the copied license file.
-/usr/local/rlm/rlm -c /opt/speedtree.lic
+/usr/local/rlm/rlm # -c /opt/speedtree.lic
